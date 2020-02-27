622bf55771b5cb81e3354888ccc9a71f435c398b
Subhash Chandra <TMaYaD@LoonyB.in>
2020-02-27 22:31:27 +0530

    Initial commit

---
+++ lib/bundler_gem_block.rb
@@ -0,0 +1,6 @@
+require "bundler_gem_block/version"
+
+module BundlerGemBlock
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bundler_gem_block/version.rb
@@ -0,0 +1,3 @@
+module BundlerGemBlock
+  VERSION = "0.1.0"
+end
